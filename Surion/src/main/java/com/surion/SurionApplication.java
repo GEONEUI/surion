2cc42e1de3cb38beed4ad28d76acab4e182628be
Yewonj927 <118143001+Yewonj927@users.noreply.github.com>
2023-04-07 14:37:41 +0900

    first order2

--- Surion/src/main/java/com/surion/SurionApplication.java
+++ Surion/src/main/java/com/surion/SurionApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SurionApplication {
+public class SurionApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(SurionApplication.class, args);
