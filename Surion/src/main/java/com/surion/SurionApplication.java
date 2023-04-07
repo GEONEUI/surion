a03331550e0c3ba130d4fb020e1e588e82d8c977
yourjean <wifilguplus@naver.com>
2023-04-07 14:49:27 +0900

    first commit

--- Surion/src/main/java/com/surion/SurionApplication.java
+++ Surion/src/main/java/com/surion/SurionApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SurionApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(SurionApplication.class, args);
 	}
 
