5b36df6b4f21dc0c9fdbd2ef76a7d2a8101b4e13
GEONEUI <dbrjsdml@naver.com>
2023-04-13 10:56:19 +0900

    테스트

--- Surion/src/main/java/com/surion/SurionApplication.java
+++ Surion/src/main/java/com/surion/SurionApplication.java
@@ -2,12 +2,14 @@ package com.surion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SurionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SurionApplication.class, args);
+		
 	}
 
 }
--- Surion/src/main/java/com/surion/controller/MyPageController.java
+++ Surion/src/main/java/com/surion/controller/MyPageController.java
@@ -1,26 +1,41 @@
 package com.surion.controller;
 
-import javax.servlet.http.HttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import lombok.extern.log4j.Log4j2;
 
 
 @Controller
 @RequestMapping("/mypage/*")
 public class MyPageController {
 	
-	@RequestMapping("/myinfo")
-	public String myinfo(Model model, HttpServletRequest request) {
+	
+	
+	@GetMapping("/myinfo")
+	public String myinfo(Model model, HttpServletRequest request, HttpSession session) {
 		String pagev = request.getParameter("pageview");
 		if(pagev == null) {
 			pagev = "1";
 		}
 		
 		int pageview = Integer.parseInt(pagev);
-		
-		model.addAttribute("pageview", pageview);
+		session.setAttribute("id", "testid");
+		model.addAttribute("pageview", pageview);	
 		return "/mypage/mypage";
 	}
+	
+	
+
+
 }
