80033cfd55f9bd1cc4c89e40bb18db18cef10991
GEONEUI <dbrjsdml@naver.com>
2023-05-03 18:23:57 +0900

    채팅기능 병합

--- Surion/src/main/java/com/surion/SurionApplication.java
+++ Surion/src/main/java/com/surion/SurionApplication.java
@@ -2,12 +2,14 @@ package com.surion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 
 @SpringBootApplication
-public class SurionApplication {
+public class SurionApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) { 
 		SpringApplication.run(SurionApplication.class, args);
@@ -16,6 +18,11 @@ public class SurionApplication {
 	@Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
+    }
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SurionApplication.class);
     }
 
 }
